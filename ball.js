543984eeaf65db84abf10d58e56162fa9f1d514f
RobTheDudeYo <robthedude@gmail.com>
2023-07-27 18:14:44 +0100

    .

---
+++ ball.js
@@ -0,0 +1,40 @@
+class Ball {
+    constructor() {
+        // set up the ball
+        this.element = document.createElement('div');
+        this.element.id = 'ball';
+        this.element.style.width = resolution / 25 + 'px';
+        this.element.style.height = resolution / 25 + 'px';
+        this.element.style.left = (resolution - parseInt(this.element.style.width)) / 2 + 'px';
+        this.element.style.top = (resolution - parseInt(this.element.style.height)) / 2 + 'px';
+        gamePanel.appendChild(this.element);
+        this.ballSize = parseInt(this.element.style.width);
+        this.ballX = parseInt(this.element.style.left);
+        this.ballY = parseInt(this.element.style.top);
+        this.velocityX = resolution / 100;
+        this.velocityY = (resolution / 100) + 1;
+    }
+
+    move() {
+        // move the dot
+        this.ballX += this.velocityX;
+        this.ballY += this.velocityY;
+        // check for collisions
+        if ((this.ballX <= 0 && this.velocityX < 0) || (this.ballX >= (resolution - this.ballSize) && this.velocityX > 0)) {
+            this.velocityX = -this.velocityX;
+        }
+        if ((this.ballY <= 0 && this.velocityY < 0) || (this.ballY >= (resolution - this.ballSize) && this.velocityY > 0)) {
+            this.velocityY = -this.velocityY;
+        }
+        // normalise the velocity
+        this.velocityMagnitude = (this.velocityX ** 2 + this.velocityY ** 2) ** 0.5;
+        this.vx_norm = this.velocityX / this.velocityMagnitude;
+        this.vy_norm = this.velocityY / this.velocityMagnitude;
+
+        this.velocityX = this.vx_norm * speed_constant;
+        this.velocityY = this.vy_norm * speed_constant;
+
+        this.element.style.left = `${this.ballX}px`;
+        this.element.style.top = `${this.ballY}px`;
+    }
+}
--- script.js
+++ script.js
@@ -10,19 +10,9 @@ gamePanel.style.left = (window.innerWidth - resolution) / 2 + 'px';
 let panelWidth = gamePanel.style.width;
 let panelHeight = gamePanel.style.height;
 
-// set up the ball
-const theBall = document.createElement('div');
-theBall.id = 'ball';
-theBall.style.width = resolution / 25 + 'px';
-theBall.style.height = resolution / 25 + 'px';
-theBall.style.left = (resolution - parseInt(theBall.style.width)) / 2 + 'px';
-theBall.style.top = (resolution - parseInt(theBall.style.height)) / 2 + 'px';
-gamePanel.appendChild(theBall);
-let ballSize = parseInt(theBall.style.width);
-let ballX = parseInt(theBall.style.left);
-let ballY = parseInt(theBall.style.top);
-let velocityX = resolution / 100;
-let velocityY = (resolution / 100) + 1;
+const speed_constant = 10;
+
+let theBall = new Ball();
 
 // listen for clicks on the game panel
 gamePanel.addEventListener('click', function () {
@@ -30,29 +20,12 @@ gamePanel.addEventListener('click', function () {
 }
 );
 
-function main() {
 
-    // check for collisions
-    if ((ballX <= 0 && velocityX < 0) || (ballX >= (resolution - ballSize) && velocityX > 0)) {
-        console.log(ballX, ballY);
-        velocityX = -velocityX;
-    }
-    if ((ballY <= 0 && velocityY < 0) || (ballY >= (resolution - ballSize) && velocityY > 0)) {
-        console.log(ballX, ballY);
-        velocityY = -velocityY;
-    }
+function mainLoop() {
 
-    // move the dot
-    ballX += velocityX;
-    ballY += velocityY;
-    theBall.style.left = `${ballX}px`;
-    theBall.style.top = `${ballY}px`;
+    theBall.move();
 
-    requestAnimationFrame(main);
-
+    requestAnimationFrame(mainLoop);
 }
 
-requestAnimationFrame(main);
-
-// I presume this means 60fps?
-// setInterval(main, 1000 / 60);
+requestAnimationFrame(mainLoop);
