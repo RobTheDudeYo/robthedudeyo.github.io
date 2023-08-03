68ca3bd44efe59ac46859eb75c16ee27be9ae187
rob <robthedude@gmail.com>
2023-08-03 18:34:10 +0100

    SERVE!

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ const ballSize = grain * 400;
 let lives = 3;
 
 const balls = [];
+const servingBalls = [];
 const blocks = [];
 
 let gameState = 'start';
@@ -16,52 +17,62 @@ class Block {
 }
 
 class Ball {
-    constructor() {
+    constructor(vX, vY, x, y) {
         // set up the ball
         this.element = document.createElement('div');
         this.element.id = 'ball';
         this.element.style.width = ballSize + 'px';
         this.element.style.height = ballSize + 'px';
-        this.x = parseInt(Math.random() * resolution);
-        this.y = parseInt(Math.random() * resolution);
+        this.serve = true;
+        this.x = x != null ? x : parseInt(Math.random() * resolution);
+        this.y = y != null ? y : parseInt(Math.random() * resolution);
         this.element.style.left = this.x + 'px';
         this.element.style.top = this.y + 'px';
-        this.velocityX = grain * Math.random() * 100;
-        this.velocityY = grain * Math.random() * 50 - 5;
+        this.velocityX = vX != null ? vX : grain * Math.random() * 100;
+        this.velocityY = vY != null ? vY : grain * Math.random() * 50 - 5;
     }
 
     move() {
-        const scaledVelocityX = (this.velocityX * deltaTime);
-        const scaledVelocityY = (this.velocityY * deltaTime);
-        // move the ball
-        this.x += scaledVelocityX;
-        this.y += scaledVelocityY;
-        this.element.style.left = `${this.x}px`;
-        this.element.style.top = `${this.y}px`;
+        if (!this.serve) {
+            const scaledVelocityX = (this.velocityX * deltaTime);
+            const scaledVelocityY = (this.velocityY * deltaTime);
+            // move the ball
+            this.x += scaledVelocityX;
+            this.y += scaledVelocityY;
+            this.element.style.left = `${this.x}px`;
+            this.element.style.top = `${this.y}px`;
 
-        // check for collisions
-        if ((this.x <= 0 && this.velocityX < 0) || (this.x >= (resolution - ballSize) && this.velocityX > 0)) {
-            this.x = (this.x <= 0 ? 0 : resolution - ballSize);
-            this.velocityX = -this.velocityX;
+            // check for collisions
+            if ((this.x <= 0 && this.velocityX < 0) || (this.x >= (resolution - ballSize) && this.velocityX > 0)) {
+                this.x = (this.x <= 0 ? 0 : resolution - ballSize);
+                this.velocityX = -this.velocityX;
+            }
+            if (this.y <= 0 && this.velocityY < 0) {
+                this.y = (this.y <= 0 ? 0 : resolution - ballSize);
+                this.velocityY = -this.velocityY;
+            } else if (this.y >= (resolution - ballSize) && this.velocityY > 0) {
+                // the ball has hit the bottom of the screen
+                // remove the element from the DOM and the balls array
+                this.serve = true;
+                servingBalls.push(this);
+                return;
+            }
+            // normalise the velocity
+            this.velocityMagnitude = (this.velocityX ** 2 + this.velocityY ** 2) ** 0.5;
+            this.vx_norm = this.velocityX / this.velocityMagnitude;
+            this.vy_norm = this.velocityY / this.velocityMagnitude;
+
+            this.velocityX = this.vx_norm * speed_constant;
+            this.velocityY = this.vy_norm * speed_constant;
+        } else {
+            // if the ball is serving, keep it on the paddle
+            this.x = thePaddle.x + thePaddle.width / 2 - ballSize / 2;
+            this.y = thePaddle.y - ballSize;
+            this.velocityX = grain * 10;
+            this.velocityY = -(grain * 10);
+            this.element.style.left = `${this.x}px`;
+            this.element.style.top = `${this.y}px`;
         }
-        if (this.y <= 0 && this.velocityY < 0) {
-            this.y = (this.y <= 0 ? 0 : resolution - ballSize);
-            this.velocityY = -this.velocityY;
-        } else if (this.y >= (resolution - ballSize) && this.velocityY > 0) {
-            // the ball has hit the bottom of the screen
-            // remove the element from the DOM and the balls array
-            this.element.remove();
-            balls.splice(balls.indexOf(this), 1);
-            return;
-        }
-        // normalise the velocity
-        this.velocityMagnitude = (this.velocityX ** 2 + this.velocityY ** 2) ** 0.5;
-        this.vx_norm = this.velocityX / this.velocityMagnitude;
-        this.vy_norm = this.velocityY / this.velocityMagnitude;
-
-        this.velocityX = this.vx_norm * speed_constant;
-        this.velocityY = this.vy_norm * speed_constant;
-
     }
 
     checkCollision(object) {
@@ -183,8 +194,10 @@ const thePaddle = new Paddle();
 function gameLoop() {
     // if the game window isn't built yet, build it
     if (!document.getElementById('gamePanel')) {
-        for (let i = 0; i < 20; i++)
+        for (let i = 0; i < 3; i++) {
             balls.push(new Ball());
+            servingBalls.push(balls[i]);
+        }
         lives = 3;
         const gamePanel = document.createElement('div');
         gamePanel.id = 'gamePanel';
@@ -244,6 +257,11 @@ document.addEventListener('keydown', (event) => {
     else if (event.key === 'Enter' && gameState === 'start') {
         gameState = 'game';
     }
+    else if (event.key === ' ' && servingBalls[0]) {
+        console.log('serve');
+        servingBalls[0].serve = false;
+        servingBalls.shift();
+    }
 });
 
 document.addEventListener('keyup', (event) => {
@@ -253,6 +271,7 @@ document.addEventListener('keyup', (event) => {
         currentDirection = 0;
     }
 });
+
 if (window.innerWidth < window.innerHeight) {
     // Add controls for mobile
     // left button and right button, below the gamePanel
