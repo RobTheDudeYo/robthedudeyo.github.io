4767227669d0d40a97c5909c045231c8188abd22
RobTheDudeYo <robthedude@gmail.com>
2023-08-08 18:33:41 +0100

    .

--- script.js
+++
@@ -1,6 +0,0 @@
-const menuButton = document.getElementById("menu-button");
-const sideMenu = document.getElementById("side-menu");
-
-menuButton.addEventListener("click", () => {
-  sideMenu.style.left = sideMenu.style.left === "0px" ? "-250px" : "0px";
-});
