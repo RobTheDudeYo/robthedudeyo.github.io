5d1c75f9ada345796746c9f4c806faa4191f5388
RobTheDudeYo <robthedude@gmail.com>
2023-08-16 18:22:55 +0100

    special blocks

--- game.js
+++ game.js
@@ -135,7 +135,7 @@ class Game {
                     level[y][x] += 9;
                 }
                 this.blocks[x][y] = new Block(this.resolution, x, y, level[y][x], this.container, this.balls, this.paddle);
-                if ((level[y][x] > 0 && level[y][x] < 8) || level[y][x] > 9) {
+                if ((level[y][x] > 0 && level[y][x] < 9) || level[y][x] > 9) {
                     this.currentLevelBlocks++;
                 }
             } // constructor(resolution, x, y, type, panel, balls, paddle)
@@ -161,7 +161,7 @@ class Game {
 class Paddle {
     constructor(resolution, panel) {
         this.resolution = resolution;
-        this.width = this.resolution / 5;
+        this.width = this.resolution / 6;
         this.height = this.resolution / 30;
         this.x = (this.resolution / 2) - (this.width / 2);
         this.y = this.resolution * 0.75;
@@ -194,10 +194,11 @@ class Ball {
         this.height = resolution / 50;
         this.x = parentBall ? parentBall.x : paddle.x;
         this.y = parentBall ? parentBall.y : paddle.y - this.height;
-        this.speed = parentBall ? parentBall.speed : resolution * 0.00025;
+        this.speed = parentBall ? parentBall.speed : resolution * 0.00015;
         this.velocity = parentBall ? { x: parentBall.velocity.x * direction.x, y: parentBall.velocity.y * direction.y } : { x: 0, y: 0 };
         this.serving = serving ? true : false;
-        this.paddleLock = (paddle.width / 2) + resolution * 0.03;
+        this.paddleLock = 0.35;
+        this.smasher = false;
         this.element = document.createElement("div");
         this.element.classList = "ball";
         this.element.style.width = this.width + "px";
@@ -221,7 +222,8 @@ class Ball {
         if (!this.serving) {
             this.collisionCheck(paddle, blocks, game, deltaTime);
         } else {
-            this.x = paddle.x + this.paddleLock;
+
+            this.x = ((paddle.x + (paddle.width / 2)) - (this.width / 2)) + (this.paddleLock * (paddle.width / 2));
             this.y = paddle.y - this.height * 0.95;
             this.element.style.left = this.x + "px";
             this.element.style.top = this.y + "px";
@@ -233,6 +235,14 @@ class Ball {
 
 
     collisionCheck(paddle, blocks, game, deltaTime) {
+
+        if (this.y + this.height < 0 && this.smasher) {
+            this.element.remove();
+            game.balls.splice(game.balls.indexOf(this), 1);
+            game.balls.push(new Ball(this.resolution, game.container, game.paddle, true));
+            return;
+        }
+
         this.x += this.velocity.x * deltaTime;
         this.y += this.velocity.y * deltaTime;
         if (this.x < 0 && this.velocity.x < 0 || this.x > this.resolution - this.width && this.velocity.x > 0) {
@@ -240,16 +250,16 @@ class Ball {
             this.x += (this.velocity.x * deltaTime) / 2;
             this.y += (this.velocity.y * deltaTime) / 2;
         }
-        if (this.y < 0 && this.velocity.y < 0) {
+        if (this.y < 0 && this.velocity.y < 0 && !this.smasher) {
             this.y = this.y < 0 ? paddle.y : (this.resolution * 0.925) - this.height;
         }
         this.element.style.left = this.x + "px";
         this.element.style.top = this.y + "px";
 
         // paddle collision
-        if (this.y > paddle.y - this.height && this.y < paddle.y && this.x > paddle.x - this.width && this.x < paddle.x + paddle.width && this.velocity.y > 0) {
+        if (this.y + this.height > paddle.y && this.y + this.height / 2 < paddle.y && this.x > paddle.x && this.x < paddle.x + paddle.width && this.velocity.y > 0) {
             if (game.sticky) {
-                this.paddleLock = this.x - paddle.x;
+                this.paddleLock = ((this.x + (this.width / 2)) - (paddle.x + (paddle.width / 2))) / (paddle.width / 2);
                 this.serving = true;
                 if (game.sticky > 0) {
                     game.sticky--;
@@ -260,6 +270,7 @@ class Ball {
                 game.multiplier = 1;
             }
             this.adjustedSpeed(paddle);
+            this.speedIncrease();
             return;
         }
 
@@ -269,58 +280,67 @@ class Ball {
         let gridY = (Math.floor((this.y + (this.width / 2)) / (this.resolution / 25))) - 1;
 
 
-        let hit = false;
         // first check current grid location, just in case we missed it otherwise
         if (gridY > 0 && gridY < 16) {
             if ((blocks[gridX][gridY].type > 0 && blocks[gridX][gridY].type < 8) || blocks[gridX][gridY].type > 9) {
-                console.log("boop")
-                this.velocity.x *= -1;
-                this.velocity.y *= -1;
-                this.x += this.velocity.x;
-                this.y += this.velocity.y;
+                console.log("this is less than ideal 1")
+                if (!this.smasher) {
+                    this.velocity.x *= -1;
+                    this.velocity.y *= -1;
+                    this.x += this.velocity.x;
+                    this.y += this.velocity.y;
+                }
                 blocks[gridX][gridY].hit(this, game);
                 return;
             }
         }
 
         // check left
-        if (gridY > 0 && gridY < 15 && gridX > 0 && !hit) {
+        if (gridY > 0 && gridY < 15 && gridX > 0) {
             if (blocks[gridX - 1][gridY].type > 0 && this.x < blocks[gridX - 1][gridY].x + blocks[gridX - 1][gridY].width && this.velocity.x < 0) {
-                this.velocity.x *= -1;
-                this.x += this.velocity.x;
-                this.y += this.velocity.y;
+                if (!this.smasher) {
+                    this.velocity.x *= -1;
+                    this.x += this.velocity.x;
+                    this.y += this.velocity.y;
+                }
                 blocks[gridX - 1][gridY].hit(this, game);
                 return;
             }
         }
         // check right
-        if (gridX < 10 && gridY > 0 && gridY < 15 && !hit) {
+        if (gridX < 10 && gridY > 0 && gridY < 15) {
             if (blocks[gridX + 1][gridY].type > 0 && this.x + this.width > blocks[gridX + 1][gridY].x && this.velocity.x > 0) {
-                this.velocity.x *= -1;
-                this.x += this.velocity.x;
-                this.y += this.velocity.y;
+                if (!this.smasher) {
+                    this.velocity.x *= -1;
+                    this.x += this.velocity.x;
+                    this.y += this.velocity.y;
+                }
                 blocks[gridX + 1][gridY].hit(this, game);
                 return;
             }
         }
         // check above
-        if (gridY > 0 && gridY < 17 && !hit) {
+        if (gridY > 0 && gridY < 17) {
             if (blocks[gridX][gridY - 1].type > 0 && this.y < blocks[gridX][gridY - 1].y + blocks[gridX][gridY - 1].height && this.velocity.y < 0) {
-                this.velocity.y *= -1;
-                this.y += this.velocity.y;
-                this.x += this.velocity.x;
+                if (!this.smasher) {
+                    this.velocity.y *= -1;
+                    this.y += this.velocity.y;
+                    this.x += this.velocity.x;
+                }
                 blocks[gridX][gridY - 1].hit(this, game);
                 return;
             }
         }
         // check below
-        if (gridY < 15 && !hit) {
+        if (gridY < 15 && gridY > 0) {
             if (blocks[gridX][gridY + 1].type > 0 &&
                 this.y + this.height * 1.1 > blocks[gridX][gridY + 1].y &&
                 this.velocity.y > 0) {
-                this.velocity.y *= -1;
-                this.y += this.velocity.y;
-                this.x += this.velocity.x;
+                if (!this.smasher) {
+                    this.velocity.y *= -1;
+                    this.y += this.velocity.y;
+                    this.x += this.velocity.x;
+                }
                 blocks[gridX][gridY + 1].hit(this, game);
                 return;
             }
@@ -328,10 +348,6 @@ class Ball {
     }
 
     adjustedSpeed(paddle) {
-        this.speed += this.resolution * 0.00001;
-        if (this.speed > this.resolution * 0.0005) {
-            this.speed = this.resolution * 0.0005;
-        }
         let paddleCenter = paddle.x + (paddle.width / 2);
         let ballCenter = this.x + (this.width / 2);
         let distance = ballCenter - paddleCenter;
@@ -342,9 +358,8 @@ class Ball {
     }
 
     speedIncrease() {
-        this.speed += this.resolution * 0.00001;
-        if (this.speed > this.resolution * 0.0005) {
-            this.speed = this.resolution * 0.0005;
+        if (this.speed < this.resolution * 0.0004) {
+            this.speed += this.resolution * 0.00004;
         }
         let angle = Math.atan2(this.velocity.y, this.velocity.x);
         this.velocity.x = Math.cos(angle) * this.speed;
@@ -386,7 +401,7 @@ class Block {
         // 2 - double points
         // 3 - sticky
         // 4 - multiball
-        // 5 - fast
+        // 5 - smasher
         // 6 - wide
         // 7 - narrow
         // 8 - slow
@@ -403,7 +418,9 @@ class Block {
             }
         } else if (this.type == 2) {
             // double points
-            game.multiplier += 1;
+            game.floatingText(this, score * 20)
+            game.score += score;
+            game.multiplier += 0.1;
             game.multiplier = Math.round(game.multiplier * 100) / 100;
             game.currentLevelBlocks -= 1;
             this.type = 0;
@@ -421,12 +438,44 @@ class Block {
             game.currentLevelBlocks -= 1;
             this.type = 0;
         } else if (this.type == 5) {
+            // smasher
+            game.floatingText(this, "Smasher!");
+            this.balls.push(new Ball(this.resolution, this.panel, this.paddle, true));
+            let ball = this.balls[this.balls.length - 1]
+            ball.paddleLock = 1;
+            ball.speed = game.resolution * 0.0004;
+            ball.smasher = true;
             game.currentLevelBlocks -= 1;
             this.type = 0;
         } else if (this.type == 6) {
+            // wide
+            game.paddle.x -= game.resolution / 16;
+            game.paddle.width = game.resolution / 4;
+            game.paddle.element.style.width = game.paddle.width + "px";
+            setTimeout(() => {
+                game.paddle.x += game.resolution / 16;
+                game.paddle.width = game.resolution / 8;
+                game.paddle.element.style.width = game.paddle.width + "px";
+            }, 5000);
             game.currentLevelBlocks -= 1;
             this.type = 0;
         } else if (this.type == 7) {
+            // narrow
+            game.paddle.x += game.resolution / 32;
+            game.paddle.width = game.resolution / 16;
+            game.paddle.element.style.width = game.paddle.width + "px";
+            setTimeout(() => {
+                game.paddle.x -= game.resolution / 32;
+                game.paddle.width = game.resolution / 8;
+                game.paddle.element.style.width = game.paddle.width + "px";
+            }, 5000);
+            game.currentLevelBlocks -= 1;
+            this.type = 0;
+        } else if (this.type == 8) {
+            // slow
+            game.balls.forEach(ball => {
+                ball.speed = game.resolution * 0.00015;
+            });
             game.currentLevelBlocks -= 1;
             this.type = 0;
         }
