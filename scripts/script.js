05e37bc460a4c689f77aa98444f727af21dfe0ee
RobTheDudeYo <robthedude@gmail.com>
2023-08-20 18:10:41 +0100

    .

--- scripts/script.js
+++ scripts/script.js
@@ -8,7 +8,7 @@ let container = document.querySelector(".panel");
 let deltaTime = 0;
 let lastTime = Date.now();
 
-let gameState = "game";
+let gameState = "startMenu";
 let startMenu = null;
 let game = null;
 let endScreen = null;
