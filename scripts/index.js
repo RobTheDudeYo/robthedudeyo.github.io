7eb5c031c4c24096819f6b3d68381a5642481a31
Rob <robthedude@gmail.com>
2023-09-11 12:05:45 +0100

    Update index.js

--- scripts/index.js
+++ scripts/index.js
@@ -101,11 +101,11 @@ function run() {
                 robs[0].parent = mainRob
             } else {
                 robs[i].draw()
-                robs[i].colour -= 10
+                robs[i].colour += 5
             }
         } else {
             robs[i].draw()
-            robs[i].colour -= 10
+            robs[i].colour += 5
         }
     }
 
