adc0b58bdb36bf0a939322db0666f69ba65fc36c
RobTheDudeYo <robthedude@gmail.com>
2023-10-20 19:47:26 +0100

    initial robsanoid merge

---
+++ scripts/robsanoid/script.js
@@ -0,0 +1,156 @@
+
+
+const resolution = document.body.clientWidth < document.body.clientHeight ? document.body.clientWidth : document.body.clientHeight;
+
+const fps = document.querySelector(".fps");
+let fpses = [];
+let canvas = document.querySelector(".panel");
+canvas.width = resolution * 0.9;
+canvas.height = resolution * 0.9;
+let context = canvas.getContext("2d");
+
+
+
+let deltaTime = 0;
+let lastTime = Date.now();
+
+let gameState = "game";
+let startMenu = null;
+let game = null;
+let endScreen = null;
+
+
+function run() {
+    deltaTime = Date.now() - lastTime;
+    lastTime = Date.now();
+    if (gameState == "startMenu") {
+        gameState = "game";
+    } else if (gameState == "game") {
+        if (game == null) {
+            game = new Game();
+        }
+        game.deltaTime = deltaTime;
+        gameState = game.run();
+    } else if (gameState == "endScreen") {
+        if (game) {
+            game = null;
+        }
+        gameState = "game"
+    }
+
+    updateFPS();
+    requestAnimationFrame(run);
+}
+
+function updateFPS() {
+    // update fps counter
+    fpses.push(1000 / deltaTime);
+    if (fpses.length > 10) {
+        fpses.shift();
+    }
+    fps.innerHTML = Math.round(fpses.reduce((a, b) => a + b) / fpses.length);
+}
+
+// keyboard controls
+document.addEventListener("keydown", (e) => {
+    if (e.key == "ArrowLeft" || e.key.toLowerCase() == "a") {
+        if (game.paddle.direction > -1) {
+            game.paddle.direction = -1;
+        }
+    }
+    if (e.key == "ArrowRight" || e.key.toLowerCase() == "d") {
+        if (game.paddle.direction < 1) {
+            game.paddle.direction = 1;
+        }
+    }
+});
+document.addEventListener("keyup", (e) => {
+    if (e.key == "ArrowLeft" || e.key.toLowerCase() == "a") {
+        if (game.paddle.direction == -1) {
+            game.paddle.direction = 0;
+        }
+    }
+    if (e.key == "ArrowRight" || e.key.toLowerCase() == "d") {
+        if (game.paddle.direction == 1) {
+            game.paddle.direction = 0;
+        }
+    }
+});
+document.addEventListener("keydown", (e) => {
+    if (e.key == " ") {
+        if (gameState == "game") {
+            game.serveBall();
+        } else if (gameState == "startMenu") {
+            gameState = "game";
+        }
+    }
+});
+
+
+
+// touch buttons
+const buttonContainer = document.createElement("div");
+buttonContainer.classList.add("buttonContainer");
+document.body.appendChild(buttonContainer);
+
+const leftButton = document.createElement("div");
+leftButton.className = "controlButton left";
+leftButton.innerHTML = "&larr;";
+buttonContainer.appendChild(leftButton);
+
+const actionButton = document.createElement("div");
+actionButton.className = "controlButton action";
+actionButton.innerHTML = "l";
+buttonContainer.appendChild(actionButton);
+
+const rightButton = document.createElement("div");
+rightButton.className = "controlButton right";
+rightButton.innerHTML = "&rarr;";
+buttonContainer.appendChild(rightButton);
+
+document.body.appendChild(buttonContainer);
+
+
+// touch button controls
+document.querySelector(".left").addEventListener("touchstart", (e) => {
+    e.preventDefault();
+    if (game.paddle.direction > -1) {
+        game.paddle.direction = -1;
+    }
+});
+document.querySelector(".left").addEventListener("touchend", (e) => {
+    e.preventDefault();
+    if (game.paddle.direction == -1) {
+        game.paddle.direction = 0;
+    }
+});
+document.querySelector(".right").addEventListener("touchstart", (e) => {
+    e.preventDefault();
+    if (game.paddle.direction < 1) {
+        game.paddle.direction = 1;
+    }
+});
+document.querySelector(".right").addEventListener("touchend", (e) => {
+    e.preventDefault();
+    if (game.paddle.direction == 1) {
+        game.paddle.direction = 0;
+    }
+});
+document.querySelector(".action").addEventListener("touchstart", (e) => {
+    e.preventDefault();
+    if (gameState == "game") {
+        game.serveBall();
+    } else if (gameState == "startMenu") {
+        gameState = "game";
+    }
+});
+document.querySelector(".panel").addEventListener("touchstart", (e) => {
+    e.preventDefault();
+});
+
+
+
+
+setTimeout(() => {
+    requestAnimationFrame(run);
+}, 100);
---
+++ scripts/robsanoid/startmenu.js
@@ -0,0 +1,67 @@
+
+
+class StartMenu {
+    constructor(container) {
+        this.container = container;
+        this.resolution = container.getBoundingClientRect().width < container.getBoundingClientRect().height ? container.getBoundingClientRect().width : container.getBoundingClientRect().height;
+        this.score = 0;
+        this.title = document.createElement("div");
+        this.title.classList.add("startMenuTitle");
+        this.title.innerHTML = "Robsanoid";
+        this.container.appendChild(this.title);
+        this.leaderboard = new Leaderboard(this.container);
+        this.buttons = document.createElement("div");
+        this.buttons.classList.add("startMenuButtons");
+        this.container.appendChild(this.buttons);
+        this.optionsButton = document.createElement("div");
+        this.optionsButton.classList.add("optionsButton");
+        this.optionsButton.innerHTML = "Options";
+        this.buttons.appendChild(this.optionsButton);
+        this.startButton = document.createElement("div");
+        this.startButton.classList.add("startButton");
+        this.startButton.innerHTML = "Start";
+        this.buttons.appendChild(this.startButton);
+        setTimeout(() => {
+            this.startButton.addEventListener("touchstart", (e) => {
+                gameState = "game";
+            });
+            this.startButton.addEventListener("click", (e) => {
+                gameState = "game";
+            });
+        }, 1);
+    }
+
+    run() {
+        return gameState;
+    }
+}
+
+class Leaderboard {
+    constructor(container) {
+        this.container = container;
+        this.resolution = container.getBoundingClientRect().width < container.getBoundingClientRect().height ? container.getBoundingClientRect().width : container.getBoundingClientRect().height;
+        this.element = document.createElement("div");
+        this.element.classList.add("leaderboard");
+        this.topText = document.createElement("div");
+        this.topText.classList.add("leaderTopText");
+        this.topText.innerHTML = "Leaderboard not implemented yet";
+        this.element.appendChild(this.topText);
+        this.container.appendChild(this.element);
+        this.leaders = [
+            { name: "Player 1", score: 100 },
+            { name: "Player 2", score: 90 },
+            { name: "Player 3", score: 80 },
+            { name: "Player 4", score: 70 },
+            { name: "Player 5", score: 60 },
+            { name: "Player 6", score: 50 },
+            { name: "Player 7", score: 40 },
+            { name: "Player 8", score: 30 },
+            { name: "Player 9", score: 20 },
+            { name: "Player 10", score: 10 },
+
+        ];
+        this.leaders.forEach((leader) => {
+            this.element.innerHTML += `<div class="leader">${leader.name}:</div><div class="leaderScore">${leader.score}</div>`;
+        });
+    }
+}
